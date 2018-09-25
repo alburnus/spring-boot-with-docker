41bbc313fdd10e6e22d7571724d5c4170bf3f3fe
Krzysztof <k.ukleja@gmail.com>
2018-09-25 23:28:09 +0200

    Init commit

---
+++ src/main/java/pl/alburnus/RestInDockerApplication.java
@@ -0,0 +1,12 @@
+package pl.alburnus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestInDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestInDockerApplication.class, args);
+	}
+}
